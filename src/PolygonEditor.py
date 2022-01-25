737cb8f6a994b901af2d4fafa64927398ed19092
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-25 19:55:32 +0100

    Create the PolygonEditor.py app structure skeleton

--- src/PolygonEditor.py
+++ src/PolygonEditor.py
@@ -9,3 +9,64 @@ import numpy.random as RandomArray
 
 import wx
 from wx.lib.floatcanvas import NavCanvas, FloatCanvas
+
+
+class DrawFrame(wx.Frame):
+    """A frame used for the Floatcanvas Demo."""
+
+    def __init__(self, parent, id, title, position, size):
+        pass
+
+    def resetSelections(self):
+        pass
+
+    def onAbout(self, event: wx.Event):
+        pass
+
+    def zoomToFit(self, event: wx.Event):
+        pass
+
+    def clear(self, event: wx.Event=None):
+        pass
+
+    def onQuit(self, event: wx.Event):
+        pass
+
+    def onCloseWindow(self, event: wx.Event):
+        pass
+
+    def onMove(self, event: wx.Event):
+        pass
+
+    def onLeftUp(self, event: wx.Event):
+        pass
+
+    def onLeftClick(self, event: wx.Event):
+        pass
+
+    def setup(self, event: wx.Event=None):
+        pass
+
+    def selectPolygon(self, obj):
+        pass
+
+    def deselectPolygon(self):
+        pass
+
+    def selectPointHit(self, pointSet):
+        pass
+
+
+class PolygonEditor(wx.App):
+    """A simple example of making editable shapes with FloatCanvas."""
+
+    def OnInit(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
