265b1ee61b9645982093357dd4a6bd74a39853ea
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-25 19:29:53 +0100

    Create the dialog.py app structure skeleton

--- src/dialog.py
+++ src/dialog.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 import wx
+
+
+class MyDialog(wx.Dialog):
+
+    def __init__(self, *args, **kw):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
