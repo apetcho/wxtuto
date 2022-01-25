a9cafa7cb206a91ad563d4e0ecc8f3827261b54c
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-25 19:19:39 +0100

    Create the events.py app structure skeleton

--- src/events.py
+++ src/events.py
@@ -5,3 +5,28 @@ from six import print_
 print_(wx.version())
 
 
+class MyFrame(wx.Frame):
+    def __init__(self, *args, **kw):
+        pass
+
+    def after(self, a, b, c):
+        pass
+
+    def onSize(self, event):
+        pass
+
+
+class MyApp(wx.App):
+    def OnInit(self):
+        pass
+
+    def OnExit(self):
+        pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
