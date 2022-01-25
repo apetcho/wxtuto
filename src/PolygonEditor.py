68decf2bdf9a147e429911cab253c56b38254090
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2022-01-25 19:48:46 +0100

    Start PolygonEditor.py

---
+++ src/PolygonEditor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""PolygonEditor: a simple app for editing polygons
+
+Used as a demo for FloatCanvas.
+"""
+import numpy as np
+import random
+import numpy.random as RandomArray
+
+import wx
+from wx.lib.floatcanvas import NavCanvas, FloatCanvas
